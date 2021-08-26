a8c0df03803f5b6e3f45235ba05cc08767e0549e
Moah-M <Moah-M>
2021-08-25 23:24:55 -0700

    ne

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - defines the main fun
+ * Return: returns null
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}

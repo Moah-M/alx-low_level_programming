6e3ceac771f6501d3f39d75a9ab4508e4a478478
Moah-M <Moah-M>
2021-08-27 10:29:06 -0700

    today's challenges

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - defines what the main purpose is
+ * Return: gives a response
+ */
+int main(void)
+{
+	int n;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+	return (0);
+}

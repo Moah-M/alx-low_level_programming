89f608c4cced968234a1e595ca54aca23ad13beb
Moah-M <Moah-M>
2021-08-27 12:33:17 -0700

    DOne

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	char lcase= 'a';
+	char lcase = 'a';
 
 	for (lcase = 'a'; lcase <= 'z'; lcase++)
 	{
 		if (lcase != 'q' && lcase != 'e')
 			putchar(lcase);
 	}
+	putchar('\n');
 	return (0);
 }

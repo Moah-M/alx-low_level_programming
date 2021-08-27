ad978f3c6b6a450547aad7d04bb6a5f71aa24a72
Moah-M <Moah-M>
2021-08-27 10:46:19 -0700

    today's challenges

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,18 +6,12 @@
  */
 int main(void)
 {
-	int n = rand();
+	int n = 98;
 	if (n > 0)
-	{
 		printf("%d is positive", n);
-	}
 	else if (n < 0)
-	{
 		printf("%d is negative", n);
-	}
 	else
-	{
 		printf("%d is zero", n);
-	}
 	return (0);
 }

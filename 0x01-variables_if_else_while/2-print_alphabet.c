050dbbb0ef9c392bfcc469b51fcf4f6ebe8963f7
Moah-M <Moah-M>
2021-08-27 12:08:15 -0700

    DOne

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdlib.h>
+/**
+ * main - Main worker
+ * Return: value passer
+ */
+int main(void)
+{
+	char lcase = 'a';
+	for (lcase= 'a'; lcase <=z; lcase++)
+	{
+		putchar(lcase);
+		putchar("\n");
+	}
+	return (0);
+}

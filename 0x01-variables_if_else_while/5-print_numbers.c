150a948cf2a2a94dcaf31cd1179bd63add7f0fb8
Moah-M <Moah-M>
2021-08-27 12:36:54 -0700

    DOne

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - definer
+ * Return: passer
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf(n);
+	printf("\n");
+	return (0);
+}
+

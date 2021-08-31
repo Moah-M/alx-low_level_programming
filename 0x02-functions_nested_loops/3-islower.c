7f3052ebc792725e43afeafbe8f577c014a5f0dc
Moah-M <Moah-M>
2021-08-31 12:29:40 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * _islower -new function
+ * @l: comm
+ * Return: 0
  */
 int _islower(int l)
 {
-	if (l >= 97 && l <=122)
+	if (l >= 97 && l <= 122)
 	{
 		return (1);
 	}

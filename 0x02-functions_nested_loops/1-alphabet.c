667ed50059d3db44bd863fe472b3020ec81f9a53
Moah-M <Moah-M>
2021-08-31 12:06:18 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function
+ * Return: done
+ */
+void print_alphabet(void)
+{
+	char low = 'a';
+
+	for (low = 'a'; low <= 'z'; low ++)
+		_putchar(low);
+	_putchar('\n');
+	return (0);

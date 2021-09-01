d57b1dc170df8582715a5fe3f24280d172bb1a9d
Moah-M <Moah-M>
2021-09-01 10:38:38 -0700

    new

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_times_table - print function
+ * @x: var 1
+ * Return: val 0
+ */
+void print_times_table(int n)
+{
+	int x, y, z;
+	if (x >= 0 && x <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar(48);
+			for (y = 1; y <= n; y++)
+			{
+				z = x * y;
+				_putchar(44);
+				_putchar(32);
+				if (z <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(z + 48);
+				}
+				else if (z <= 99)
+				{
+					_putchar(32);
+					_putchar((z / 10) + 48);
+					_putchar((z % 10) + 48);
+				}
+				else
+				{
+					_putchar(((z / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((z % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}

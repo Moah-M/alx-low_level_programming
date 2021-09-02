77feb2c6682ca7a303c52685688423777146e0ae
Moah-M <Moah-M>
2021-09-01 22:30:39 -0700

    my commits

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * main - defines what the main purpose is
  * Return: gives a response
  */
 int main(void)
 {
-	int n = rand() - RAND_MAX / 2;
+	int i;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-	return (0);
+	positive_or_negative(i);
+	return (0);	
 }

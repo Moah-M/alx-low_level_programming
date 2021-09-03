7ef9ebf3eeadfa5e368af5d196cba246b32eb649
Moah-M <Moah-M>
2021-09-03 10:07:31 -0700

    finito

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main - defines what the main purpose is
+ * here is another line of command
  * Return: gives a response
  */
 int main(void)
@@ -8,6 +9,8 @@ int main(void)
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
+
 	return (0);
 }

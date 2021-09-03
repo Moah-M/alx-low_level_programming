081e8804d777e25b9878ebdadd8137913e402099
Moah-M <Moah-M>
2021-09-03 13:07:24 -0700

    dd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
  * Return: 0
  */
 
-void print_number(void)
+void print_numbers(void)
 {
 	int n;
 
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,35 @@ void more_numbers(void)
 {
 	int n;
 	char x;
-	
-	for (n = '0'; n<= '9'; n++)
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (x = 0; x <= 14; x++)
 		{
-			for (x = 0; x <= 14; x++)
-			{
-				if (x > 9)
-					_putchar((x / 10) + '0');
-					_putchar((x % 10) + '0');
-			}
-			_putchar('\n');
+			_putchar(x);#include "main.h"
+
+/**
+ * more_numbers - function
+ */
+
+void more_numbers(void)
+{
+        int n;
+        char x;
+
+        for (n = '0'; n <= '9'; n++)
+        {
+                for (x = 0; x <= 14; x++)
+                {
+                        _putchar(x);
+                }
+                _putchar('\n');
+        }
+
+
+
+
 		}
+		_putchar('\n');
+	}
 }

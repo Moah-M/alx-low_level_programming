fc03488f2997b9817553801522b38f633235575b
Moah-M <Moah-M>
2021-09-06 04:09:57 -0700

    today's changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 /**
  * reset_to_98 - main function
  * Return: 0
  */
 
-void rest_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n=98;
 }

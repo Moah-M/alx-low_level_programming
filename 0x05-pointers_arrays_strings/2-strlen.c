40a74fdd231477722561ca954ddc004887e4e21e
Moah-M <Moah-M>
2021-09-06 07:01:23 -0700

    defn

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,13 @@
 /**
  * _strlen - main function
  * @s: var 1
- * @x: var 2
+ * @y: var 2
  * Return: 0
  */
 int _strlen(char *s)
 {
-	int x;
-
-	for (x = 0; x != '\0'; x++)
-	{
-		return (x);
-	}
+	int  = 0;
+	while (*s++ != '\0')
+		i++;
+	return (y);
 }

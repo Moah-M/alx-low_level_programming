d454fbffc44d1bc02678a8a80870903adf592cba
Moah-M <Moah-M>
2021-09-09 05:50:57 -0700

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,19 @@
  * _strncpy - main func
  * @dest: var 1
  * @src: var 2
- * @n: num 1
+ * @n: num
  * Return: 0
  */
 
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, op = 0;
+	int i;
 
-	while (op == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
-	}
-	return (op);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }

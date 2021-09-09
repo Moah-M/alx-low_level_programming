2a1430339cd1cab7c95729e8c78a257e96b3b6cb
Moah-M <Moah-M>
2021-09-09 09:50:17 -0700

    ds

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,7 +6,7 @@
  */
 void print_number(int n)
 {
-	unsigned int m, d, c;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
@@ -19,17 +19,17 @@ void print_number(int n)
 	}
 
 	d = m;
-	c = 1;
+	count = 1;
 
 	while (d > 9)
 	{
 		d /= 10;
-		c *= 10;
+		count *= 10;
 	}
 
-	for ( ; c >= 1; c /= 10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / c) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
 

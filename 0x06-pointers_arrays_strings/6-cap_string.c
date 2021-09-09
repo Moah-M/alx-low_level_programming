2b7dc93cd4f25c2b243ad02b9f94df5f42bfd52e
Moah-M <Moah-M>
2021-09-09 08:37:43 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if(*(s + x) == sep[i])
+			if (*(s + x) == sep[i])
 			{
 				if ((*(s + (x + 1)) >= 97) && (*(s + (x + 1)) <= 122))
 					*(s + (x + 1)) = *(s + (x + 1)) - 32;

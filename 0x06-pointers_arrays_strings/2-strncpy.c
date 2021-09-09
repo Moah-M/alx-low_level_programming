84d28d545548224589d830a10026ff96765344ca
Moah-M <Moah-M>
2021-09-09 05:37:02 -0700

    defn

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - main func
+ * @dest: var 1
+ * @src: var 2
+ * @n: num 1
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i=0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (op);
+}

72e9d5f6df093fc4ec8d26fc4126f50ba03a7fc7
Moah-M <Moah-M>
2021-09-10 05:08:12 -0700

    defr

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+/**char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -13,5 +13,5 @@ char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+void print_buffer(char *b, int size);*/
 #endif

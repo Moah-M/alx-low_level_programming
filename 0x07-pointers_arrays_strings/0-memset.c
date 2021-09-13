1c947fb96d592571e9169fcc3e5fe38b0d6530e7
Moah-M <Moah-M>
2021-09-12 22:42:18 -0700

    de

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,16 @@
 #include "main.h"
+/**
+ * _memset - main function
+ * 
+ * Return: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
 
-
+	while (i < n)
+	{
+		s[i] = n;
+	}
+	return (s);
+}

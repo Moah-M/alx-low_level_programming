5b5ec113689d117e82ab591bf43569ebb3c842e3
Moah-M <Moah-M>
2021-09-15 00:10:00 -0700

    ds

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,13 @@ int _strlen_recursion(char *s)
 {
 	int i, count = 0;
 
-	if(i = 0)
+	if (i = 0)
 	{
 		return count;
 	}
 	else
 	{
-		return _strlen_recursion((s + i))
+		return _strlen_recursion((s + i));
 	}
 	return count;
 }

286b3525931b13224c18997ffffabcec05e6604c
Moah-M <Moah-M>
2021-09-15 00:27:12 -0700

    newr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,10 @@
 /**
  * _strlen_recursion - main function
  * @s: var 1
- * @count: var 2
  * Return: 0
  */
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s != '\0')
 	{
 		return (1 +_strlen_recursion(s + 1));

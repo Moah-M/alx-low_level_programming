452f82ba8a555ce047aa41bd3a631f29f2138630
Moah-M <Moah-M>
2021-09-15 00:17:27 -0700

    new

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,21 +3,12 @@
 /**
  * _print_rev_recursion - main
  * @s: var 1
- * @i: vae 2
- * Return: 0
  */
 void _print_rev_recursion(char *s)
 {
-	int i = sizeof(s);
-	i--;
-	if (i == 1)
+	if(*s != '\0')
 	{
-		return;
-	}
-	else
-	{
-		_print_rev_recursion((s + i));
-		printf("%s", (s + i));
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
-

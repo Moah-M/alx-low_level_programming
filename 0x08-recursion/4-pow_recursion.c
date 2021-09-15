15d7c94cab4820f599ffc3a4fef257488bb3688f
Moah-M <Moah-M>
2021-09-15 00:45:16 -0700

    done 3

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _pow_recursion: main
+ * @x: var 1
+ * @y: var 2
+ * Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	return (x ** y);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,7 @@ void _print_rev_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+int sqrt_recursion(int n);
+int is_prime_number(int n);
 #endif

58ac9b2e22678efb8cc0f53786094640c1a503fd
Moah-M <Moah-M>
2021-09-15 00:38:51 -0700

    new def

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,20 @@
 /**
  * factorial - main
  * Return: 0
+ * @n: var 1
  */
 int factorial(int n)
 {
-	if (n == 1)
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
 		return (1);
+	}
 	else
 	{
-		return n * factorial(n-1);
+		return (n * factorial(n-1));
 	}
 }
-

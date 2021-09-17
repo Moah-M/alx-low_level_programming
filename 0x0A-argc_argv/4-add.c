62f7698402bafc87913f0d5188565b883f14e2eb
Moah-M <Moah-M>
2021-09-17 05:38:46 -0700

    new

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - func
+ * @argc: var 1
+ * @argv: var 2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0, i;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i]) >= 0)
+		{
+			x += atoi(argv[i]);
+		}
+		else
+		{
+			printf("0\n");
+		}
+	}
+	printf("%d\n", x);
+	return (1);
+}

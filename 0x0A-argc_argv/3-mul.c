92143dc1020506943b187c14851a0125cb145f22
Moah-M <Moah-M>
2021-09-17 03:59:27 -0700

    latest

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
  */
 int main (int argc, char *argv[])
 {
-	int i;
-
 	if (argc == 3)
 	{
 		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return(1);
 	}
 	else
 	{
 		printf("Error");
+		return (0);
 	}
 }

c40f09df88abc8242fc40c4bb0b79af87a5c4f97
Moah-M <Moah-M>
2021-09-16 21:41:49 -0700

    committed

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: var 1
+ * @argv: var 2
+ * Return: 0;
+ */
+int main(int argc, char ** argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}

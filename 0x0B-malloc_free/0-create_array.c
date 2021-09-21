f2e886f6925f79b22bebb12a9f063839ba3917b0
Moah-M <Moah-M>
2021-09-20 23:19:54 -0700

    first for today

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * creatae_array - main
@@ -8,10 +9,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char x[100];
+	char *x;
 	unsigned int i;
 
-	c == 'H';
+	x = malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
@@ -19,12 +20,8 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
-		{
-			return (*(x + i) == c);
-		}
+		return (*(x + i) == c);
 	}
-	return (*(x + i) == c);
+	return (1);
 }
-
 

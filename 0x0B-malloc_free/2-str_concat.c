b29d063670225f46409b33284b900f4e03c7132c
Moah-M <Moah-M>
2021-09-21 05:47:33 -0700

    never

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - main
+ * Return: 0
+ * @str: var 1
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	char *x;
+	int i, j, k;
+	int count = 0;
+
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		count++;
+		j++;
+	}
+	k = 0;
+	while (s2[k] != '\0')
+	{
+		count++;
+		k++
+	}
+
+	p = (char *)malloc(count * sizeof(char) + 1);
+	if (p == NULL)
+	{
+		printf("Error");
+	}
+	else
+	{
+		i = 0;
+		while (i <= count)
+		{
+			p[i] = s1[i];
+			p[i] = p[i] + s2[i];
+			i++;
+		}
+	}
+	return (p);
+}
+

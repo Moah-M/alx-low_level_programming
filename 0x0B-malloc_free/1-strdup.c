03a7bced7d7914e883f34c33acca372ddcfadf1e
Moah-M <Moah-M>
2021-09-21 03:24:49 -0700

    new addtion

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strdup - main
+ * Return: 0
+ * @str: var 1
+ */
+char *_strdup(char *str)
+{
+	char *p;
+
+	p = (char *)malloc(10 * sizeof(char));
+	if (p == NULL)
+	{
+		printf("Error");
+	}
+	else
+	{
+	return (str);
+	}
+}
+

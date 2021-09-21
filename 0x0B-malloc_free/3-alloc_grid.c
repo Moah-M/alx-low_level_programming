b267d2d36e746fa405794777b8b6b7c8dddd752e
Moah-M <Moah-M>
2021-09-21 09:12:37 -0700

    nev

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,20 +7,20 @@
  * @height: var 2
  * Return 0;
  */
-int **alloc_grid(int width, int height)
+int **alloc_grid(int width = 0, int height = 0)
 {
+	int *p;
 	int i, j;
-	width = 0, height = 0;
 	
-	for (i = 0; i < width; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d ", width);
 
-		for (j = 0; j < height; j++)
+		for (j = 0; j < 10; j++)
 		{
 			printf("%d", height);
 		}
 	}
-	return (1);
+	return ();
 }
 

df2e26b559d83959a6e4c822453ca2c9f4395e79
Moah-M <66769085+Moah-M@users.noreply.github.com>
2021-09-27 19:13:30 +0300

    Second attempt

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * init_dog - main
+ * @d: var 1
+ * @name: var 2
+ * @age: var 3
+ * @owner: var 4
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)

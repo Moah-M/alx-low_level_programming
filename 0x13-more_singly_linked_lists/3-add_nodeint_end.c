c0407393729bbbcc16ebc273ba29e63db65a1ab6
Moah-M <menelik.solomon@yahoo.com>
2021-10-12 18:56:00 +0300

    added fourth task

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - main
+ * @head: var 1
+ * @n: var 2
+ * Return: main
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *tmp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	tmp = *head;
+
+	if (tmp == NULL)
+		*head = new;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+
+		tmp->next = new;
+	}
+
+	return (*head);
+}

2c0f260f1febd62d76562220fe5cec809f673821
Moah-M <menelik.solomon@yahoo.com>
2021-10-14 18:46:21 +0300

    Sixth commit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count;
 
-	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	for (count = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			count;

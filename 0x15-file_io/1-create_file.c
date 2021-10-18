71f9f28222f4b76e64e455892424fd721345a738
Moah-M <menelik.solomon@yahoo.com>
2021-10-18 20:35:03 +0300

    Second for today

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,11 +12,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1) 
 		return (-1);
-
 	if (text_content)
 	{
 		while (text_content[words] != '\0')
@@ -30,5 +28,4 @@ int create_file(const char *filename, char *text_content)
 	close(file);
 	return (1);
 }
-
 

61bb678e2e9fe8364c31c74a8fb9560b24fc598a
Moah-M <menelik.solomon@yahoo.com>
2021-10-23 12:40:49 +0300

    TOday at last

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	long int i;
-	long int n = 95;
+	long int n = 98;
 	long int t1 = 1, t2 = 1;
 	long int nextTerm = t1 + t2;
 
 	printf("%ld, ", t2);
 
-	for (i = 3; i <= n; ++i)
+	for (i = 3; i < n; ++i)
 	{
 		printf("%ld, ", nextTerm);
 		t1 = t2;

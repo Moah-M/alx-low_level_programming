f9b2cc78de7a771538b55919df33957cd82ec571
Moah-M <menelik.solomon@yahoo.com>
2021-10-23 10:53:00 +0300

    push -u origin main

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,17 +4,17 @@
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	long int i;
 	long int n = 98;
 	long int t1 = 1, t2 = 1;
 	long int nextTerm = t1 + t2;
 
-	printf("%ld, %ld, ", t1, t2);
+	printf("%ld, ", t2);
 
-	for (i = 3; i <= n; ++i) 
-  	{
+	for (i = 3; i <= n; ++i)
+	{
 		printf("%ld, ", nextTerm);
 		t1 = t2;
 		t2 = nextTerm;

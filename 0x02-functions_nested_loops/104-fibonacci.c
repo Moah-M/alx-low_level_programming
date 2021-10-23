52da33f63a25fd5ff0afaaf4db93374382158b71
Moah-M <menelik.solomon@yahoo.com>
2021-10-23 10:49:00 +0300

    dealt with

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+/**
+ * main - main function
+ * Return: 0
+ */
 
 int main()
 {
 	long int i;
 	long int n = 98;
-	long int t1 = 0, t2 = 1;
+	long int t1 = 1, t2 = 1;
 	long int nextTerm = t1 + t2;
 
 	printf("%ld, %ld, ", t1, t2);
@@ -17,6 +20,7 @@ int main()
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
 	}
+	printf("\n");
 	return (0);
 }
 

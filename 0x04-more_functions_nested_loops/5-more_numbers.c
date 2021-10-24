370f97e475fa2c802749f9337aa626cceba0bf37
Moah-M <menelik.solomon@yahoo.com>
2021-10-24 12:05:38 +0300

    added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,6 @@
 
 void more_numbers(void)
 {
-	int n;
-	char x;
-
-	for (n = '0'; n <= '9'; n++)
-	{
-		for (x = 0; x <= 14; x++)
-		{
-			_putchar(x);#include "main.h"
-
-/**
- * more_numbers - function
- */
-
-void more_numbers(void)
-{
         int n;
         char x;
 
